b50e06b04ef186cea32fa3ac1e4901b4623d4df4
DarshanBarbhai <darshanb@outlook.com>
2017-04-02 12:13:12 -0400

    Added some helper functions.

--- filesystem.c
+++ filesystem.c
@@ -88,3 +88,109 @@ int check_block(unsigned char * disk, int curr_block, char * dir_name){
 
     return -1;
 }
+
+void make_dir(unsigned char *inode_bitmap, unsigned char *block_bitmap, char *path, unsigned char *disk, struct ext2_inode *it){
+    int num_dirs;
+    char ** seperated_path = make_path(path, &num_dirs);
+
+    int num_checked_dirs = 0, i;
+    int not_done = 1;
+    int curr_inode = 2, parent_inode = -1;
+    int curr_block;
+
+    struct ext2_super_block *sb = (struct ext2_super_block *)(disk + 1024);
+    int num_inodes = sb->s_inodes_count;
+    int num_blocks = sb->s_blocks_count;
+
+    while(not_done){
+        curr_block = it[curr_inode - 1].i_block[0];
+
+        int temp = curr_inode;
+        curr_inode = check_block(disk, curr_block, seperated_path[num_checked_dirs]);
+
+        num_checked_dirs++;
+
+        if(num_checked_dirs == num_dirs){
+            not_done = 0;
+            if(curr_inode){
+                perror("Path already exists.\n");
+                exit(EEXIST);
+            }
+        }else if(curr_inode == -1){
+            perror("Path does not exist.\n");
+            exit(ENOENT);
+        }else{
+            parent_inode = temp;
+        }
+    }
+
+
+    int free_inode = get_empty_inode(inode_bitmap, num_inodes);
+    int free_block = get_empty_data_block(block_bitmap, num_blocks);
+
+    struct ext2_inode * ind = it + (sizeof(struct ext2_inode) * (free_inode - 1));
+    struct ext2_dir_entry * new_block =  (struct ext2_dir_entry *) (disk + (free_block * EXT2_BLOCK_SIZE));
+
+    
+    struct ext2_inode *parent_ind = it + (sizeof(struct ext2_inode) * (parent_inode - 1));
+    
+    //char *names[3] = {".", "..", seperated_path[num_checked_dirs - 1]};
+    new_block->rec_len = 12;
+    new_block->name_len = 1;
+    new_block->inode = free_inode;
+    strcpy(new_block->name, ".");
+
+    new_block += new_block->rec_len;
+
+    new_block->rec_len = 1012;
+    new_block->name_len = 2;
+    new_block->inode = parent_inode;
+    strcpy(new_block->name, "..");
+
+    block_bitmap[(free_block - 1) / 8] |= (1 << (free_block - 1) % 8);
+
+    ind->i_mode = (ind->i_mode & 0) | (EXT2_S_IFDIR);
+    ind->i_block[0] = free_block;
+    ind->i_size = EXT2_BLOCK_SIZE;
+    ind->i_blocks = 1;
+    ind->i_links_count = 2;
+
+    inode_bitmap[(free_inode - 1) / 8] |= (1 << (free_inode - 1) % 8);
+
+    parent_ind->i_links_count += 1;
+
+    struct ext2_dir_entry *curr_dir;
+    int rec_length = 0;
+
+    for(i = 0; i < 15; i++){
+        
+        if(parent_ind->i_block[i]){
+            curr_dir = (struct ext2_dir_entry *) (disk + parent_ind->i_block[i] * EXT2_BLOCK_SIZE);
+            while(rec_length < EXT2_BLOCK_SIZE){
+                if(rec_length + curr_dir->rec_len >= EXT2_BLOCK_SIZE && curr_dir->rec_len != curr_dir->name_len + 12 - (curr_dir->name_len % 4) && curr_dir->rec_len != curr_dir->name_len + 8){
+                    if(curr_dir->name_len % 4 != 0){
+                        curr_dir->rec_len = curr_dir->name_len + 12 - (curr_dir->name_len % 4);
+                    }else{
+                        curr_dir->rec_len = curr_dir->name_len + 8;
+                    }
+
+                    rec_length += curr_dir->rec_len;
+                    curr_dir += curr_dir->rec_len;
+
+                    curr_dir->inode = free_inode;
+                    curr_dir->rec_len = EXT2_BLOCK_SIZE - rec_length;
+                    curr_dir->name_len = strlen(seperated_path[num_dirs - 1]);
+                    strcpy(curr_dir->name, seperated_path[num_dirs - 1]);
+                    curr_dir->file_type = EXT2_FT_DIR;
+                    rec_length += curr_dir->rec_len;
+                    return;
+                }
+                else{
+                    rec_length += curr_dir->rec_len;
+                    curr_dir += curr_dir->rec_len;
+                }
+            }
+        }
+    }
+
+}
--- filesystem.h
+++ filesystem.h
@@ -5,3 +5,9 @@
 
 int get_empty_data_block(unsigned char *block_bitmap, int blocks_count);
 int get_empty_inode(unsigned char *inode_bitmap, int blocks_count);
+
+int string_size(char *c);
+void split_path(char * path, char ** result);
+char ** make_path(char *path, int *num_directories);
+int check_block(unsigned char * disk, int curr_block, char * dir_name);
+void make_dir(unsigned char *inode_bitmap, unsigned char *block_bitmap, char *path, unsigned char *disk, struct ext2_inode *it);
